7bf38de7f71493c174a17759c4c03fa80023453e
Niels Demeyer <niels.demeyer99@gmail.com>
2024-04-15 14:24:41 +0200

    created red table

--- classes/DatabaseManager_file.py
+++ classes/DatabaseManager_file.py
@@ -18,11 +18,10 @@ class DatabaseManager:
         self.conn.close()
     
     def create_table_links(self, table_name):
-        query = f"CREATE TABLE IF NOT EXISTS {table_name} (
+        query = f"""CREATE TABLE IF NOT EXISTS {table_name} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             link TEXT NOT NULL UNIQUE
-            )"
+            )"""
         self.execute(query)
         self.conn.commit()
-    
     
