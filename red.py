07e989b6de0f3f2ed609a5140f2fbfaf291da4db
Niels Demeyer <niels.demeyer99@gmail.com>
2024-04-17 17:05:25 +0200

    cleaning

--- red.py
+++
@@ -1,9 +0,0 @@
-from classes.DatabaseManager_file import DatabaseManager
-from classes.ScraperClassFile_file import ScraperClass
-db = DatabaseManager('wine.db')
-scraper = ScraperClass()
-url_red = "https://www.vivino.com/explore?e=eJzLLbI1VMvNzLM1UMtNrLA1NTBQS660dXJVS7Z1DQ1SKwDKpqfZliUWZaaWJOao5Rel2KakFier5SdV2ialFpfEF2QmZxerlZdEx9oaAgC5yxm5"
-
-db.create_table_links('red_links')
-links = scraper.scrape(url_red)
-print(links)
